e85cb6a15ac64091f04d9f811ee1c9da60bd73d1
preston176 <prestonnyamweya@gmail.com>
2023-06-26 19:54:40 +0300

    placeholders

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,6 +3,7 @@ import './Main.css'
 
 function Main() {
   return (
+    <div className='main-wrapper'>
     <div className='main-section'>
         <p>Your Projects</p>
         <div className='main-cards'>
@@ -13,28 +14,6 @@ function Main() {
                 <div className='card-footer'>
                     <h1>cc</h1>
                 </div>
-
-                
-            </div>
-            <div className='card'>
-                <h1>Super Cool Project</h1>
-                <p>lorem
-                </p>
-                <div className='card-footer'>
-                    <h1>cc</h1>
-                </div>
-
-                
-            </div>
-            <div className='card'>
-                <h1>Super Cool Project</h1>
-                <p>lorem
-                </p>
-                <div className='card-footer'>
-                    <h1>cc</h1>
-                </div>
-
-                
             </div>
 
             <div className='card'>
@@ -44,13 +23,86 @@ function Main() {
                 <div className='card-footer'>
                     <h1>cc</h1>
                 </div>
-
-                
+            </div>
+            <div className='card'>
+                <h1>Super Cool Project</h1>
+                <p>lorem
+                </p>
+                <div className='card-footer'>
+                    <h1>cc</h1>
+                </div>
+            </div>
+            <div className='card'>
+                <h1>Super Cool Project</h1>
+                <p>lorem
+                </p>
+                <div className='card-footer'>
+                    <h1>cc</h1>
+                </div>
+            </div>
+            <div className='card'>
+                <h1>Super Cool Project</h1>
+                <p>lorem
+                </p>
+                <div className='card-footer'>
+                    <h1>cc</h1>
+                </div>
+            </div>
+            <div className='card'>
+                <h1>Super Cool Project</h1>
+                <p>lorem
+                </p>
+                <div className='card-footer'>
+                    <h1>cc</h1>
+                </div>
+            </div>
+            <div className='card'>
+                <h1>Super Cool Project</h1>
+                <p>lorem
+                </p>
+                <div className='card-footer'>
+                    <h1>cc</h1>
+                </div>
             </div>
         </div>
+    </div>
+<div className='trends-announcements'>
+    <div className='announcements-section'>
+        <p>Announcements</p>
+        <div className='announcement-card-wrapper'>
+            <div className='announcement-card'>
+                <h1>Heading</h1>
+                <p>Body</p>
+                <hr />
+            </div>
+        </div>
+    </div>
 
-        
 
+
+    <div className='trending-section'>
+        <p>Trending</p>
+        <div className='trending-card'>
+            <div className='trend-card'>
+                <img src='#'></img>
+                <div className='trend-card-texts'>
+                    <p>@tegan</p>
+                    <p>World Peace Builder</p>
+                </div>
+            </div>
+        </div>
+        <div className='trending-card'>
+            <div className='trend-card'>
+                <img src='#'></img>
+                <div className='trend-card-texts'>
+                    <p>@tegan</p>
+                    <p>World Peace Builder</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    </div>
     </div>
   )
 }
