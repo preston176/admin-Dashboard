eb66402babb12aac51a1b1d0dab2fe95da7a8807
preston176 <prestonnyamweya@gmail.com>
2023-06-26 15:21:34 +0300

    sidebar done

---
+++ src/components/sidebar.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import './Sidebar.css'
+
+function Sidebar() {
+  return (
+    <div className='sidebar-wrapper'>
+        <div className='Head-section'>
+            <img src='#'></img>
+            <p>Dashboard</p>
+            </div>
+
+    <div className='sidebar-content'>
+        <ul>
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+        </ul>
+    </div>
+
+    <div className='sidebar-content'>
+        <ul>
+            <li>Settings</li>
+            <li>Support</li>
+            <li>Privacy</li>
+        </ul>
+    </div>
+        
+        
+    </div>
+  )
+}
+
+export default Sidebar
